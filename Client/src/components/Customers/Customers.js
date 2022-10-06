dd3e28aceb12ec72edf8c71d503ef183e8e1df49
eslamicoo912 <eslamicoo3@gmail.com>
2022-10-06 19:53:53 +0200

    create fetchData functionality for the customers component

--- Client/src/components/Customers/Customers.js
+++ Client/src/components/Customers/Customers.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const URL = "http://localhost:5000/customers";
 
 const Customers = () => {
-  return <div>Customers</div>;
+  const [customers, setCustomers] = useState([]);
+
+  const fetchData = async () => {
+    const response = await axios.get(URL);
+    setCustomers(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <div className="customers"></div>;
 };
 
 export default Customers;
