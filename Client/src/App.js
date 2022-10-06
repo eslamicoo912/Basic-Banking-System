bf7682372aa9881d9ad8508a58f0ef6f47b88e5a
eslamicoo912 <eslamicoo3@gmail.com>
2022-10-06 19:27:56 +0200

    setting up routes for the client side

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Customers from "./components/Customers/Customers";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Transfer from "./components/Transfer/Transfer";
+import Transactions from "./components/Transactions/Transactions";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/transfer" element={<Transfer />} />
+          <Route path="/transactions" element={<Transactions />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
