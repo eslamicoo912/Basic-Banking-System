f9a78e71f7f05adc149374df44613f0ceb62cb0f
eslamicoo912 <eslamicoo3@gmail.com>
2022-10-06 19:46:04 +0200

    finishing the header component

--- Client/src/components/Header/Header.js
+++ Client/src/components/Header/Header.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="title">
+        <h1>
+          Basic <span>Banking</span> System
+        </h1>
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/customers">Customers</Link>
+        <Link to="/transfer">Transfer</Link>
+        <Link to="/Transactions">Transactions</Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
