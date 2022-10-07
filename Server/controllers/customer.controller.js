f880a2c7ddaac5b001ccbd9805c4daee317806ce
eslamicoo912 <eslamicoo3@gmail.com>
2022-10-07 13:40:44 +0200

    creating the transfer request for the server side

--- Server/controllers/customer.controller.js
+++ Server/controllers/customer.controller.js
@@ -27,3 +27,52 @@ export const viewOneCustomer = async (req, res) => {
     console.log(error.message);
   }
 };
+
+/*
+export const transfer = async (req, res) => {
+  const sender = { email: req.body.sender };
+  const reciever = { email: req.body.reciever };
+
+  try {
+    const customerSender = await CustomerModel.findOneAndUpdate(sender, {
+      balance: 99999 - req.body.amount,
+    });
+    const customerReciever = await CustomerModel.findOneAndUpdate(reciever, {
+      balance: 99999 + req.body.amount,
+    });
+    res.json({
+      sender: customerSender,
+      reviever: customerReciever,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};*/
+
+export const transfer = async (req, res) => {
+  const senderEmail = req.body.sender;
+  const recieverEmail = req.body.reciever;
+  try {
+    const sender = await CustomerModel.findOne({ email: senderEmail });
+    const reciever = await CustomerModel.findOne({ email: recieverEmail });
+    const amount = parseInt(req.body.amount);
+
+    const updatedSender = await CustomerModel.findOneAndUpdate(
+      { email: senderEmail },
+      { balance: sender.balance - amount },
+      { new: true }
+    );
+    const updatedReciever = await CustomerModel.findOneAndUpdate(
+      { email: recieverEmail },
+      { balance: reciever.balance + amount },
+      { new: true }
+    );
+
+    res.json({
+      sender: updatedSender,
+      reciever: updatedReciever,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- Server/routes/customer.routes.js
+++ Server/routes/customer.routes.js
@@ -4,6 +4,7 @@ import * as controllers from "../controllers/customer.controller.js";
 const routes = Router();
 
 routes.post("/", controllers.createCustomer);
+routes.post("/transfer", controllers.transfer);
 routes.get("/", controllers.viewAllCustomers);
 routes.get("/:name", controllers.viewOneCustomer);
 
